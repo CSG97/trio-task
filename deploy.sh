a8c4e4df2f4c400bd7898a9195663111e8c4ae9c
CSG97 <95858597+CSG97@users.noreply.github.com>
2021-12-09 15:35:57 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,30 @@
+#!/bin/bash
 
+#create network
+docker network create trio-task-network
+
+#Build Images
+docker build -t trio-db db
+docker build -t trio-flask-app flask-app
+
+
+#Run database Container
+docker run -d \
+  --network trio-task-network \
+  --name mysql \
+  trio-db
+
+
+#Run flask-app container
+docker run -d \
+  --network trio-task-network \
+  --name flask-app \
+  trio-flask-app
+
+#Run nginx container
+docker run -d \
+ --network trio-task-network \
+ --mount type=bind,source=$(pwd)/nginx/nginx.conf,target=/etc/nginx/nginx.conf \
+ -p 80:80 \
+ --name nginx \
+ nginx:alpine
